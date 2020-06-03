66420a9a438130dcf46d27c15a72d61564754d28
Irina Popa <irina.oana.popa@gmail.com>
2020-06-03 23:39:41 +0300

    map appeared after many issues

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
 
+const nodeModulesLoader = environment.loaders.get('nodeModules')
+if (!Array.isArray(nodeModulesLoader.exclude)) {
+  nodeModulesLoader.exclude = (nodeModulesLoader.exclude == null)
+    ? []
+    : [nodeModulesLoader.exclude]
+}
+nodeModulesLoader.exclude.push(/mapbox-gl/) // replace `some-library` with
+                                               // the actual path to exclude
 module.exports = environment

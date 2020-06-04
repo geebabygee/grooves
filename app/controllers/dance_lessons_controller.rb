52c8089eadb613723b559398159d69df0d3f9c82
Irina Popa <irina.oana.popa@gmail.com>
2020-06-04 19:01:44 +0300

    pages controller

--- app/controllers/dance_lessons_controller.rb
+++ app/controllers/dance_lessons_controller.rb
@@ -1,36 +1,36 @@
 class DanceLessonsController < ApplicationController
 
-    before_action :set_dance_lesson, only: [:show]
-    skip_before_action :authenticate_user!, only: :index
+  before_action :set_dance_lesson, only: [:show]
+  skip_before_action :authenticate_user!, only: :index
 
 
-    def index
-      @dance_lessons = DanceLesson.geocoded
+  def index
+    @dance_lessons = DanceLesson.geocoded
 
-          @markers = @dance_lessons.map do |lesson|
-            {
-              lat: lesson.latitude,
-              lng: lesson.longitude,
-              infoWindow: render_to_string(partial: "info_window", locals: { dance_lesson: lesson }),
-              image_url: helpers.asset_url('shoe.jpeg')
+    @markers = @dance_lessons.map do |lesson|
+      {
+        lat: lesson.latitude,
+        lng: lesson.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { dance_lesson: lesson }),
+        image_url: helpers.asset_url('shoe.jpeg')
 
-            }
-      end
+      }
     end
+  end
 
-    def show
-    end
+  def show
+  end
 
 
-    private
+  private
 
-    def set_dance_lesson
-      @dance_lesson = DanceLesson.find(params[:id])
-    end
+  def set_dance_lesson
+    @dance_lesson = DanceLesson.find(params[:id])
+  end
 
-    def article_params
-      params.require(:dance_lesson).permit(:name, :description, :level, :photo)
-    end
+  def article_params
+    params.require(:dance_lesson).permit(:name, :description, :level, :photo)
+  end
 end
 
 
